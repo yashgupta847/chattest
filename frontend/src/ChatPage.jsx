5c3dad92aefff966380a5d99222e6e3cf6796be1
yash gupta <yashgupta5046@gmail.com>
2025-04-09 23:43:24 +0530

    2nd commit

--- frontend/src/ChatPage.jsx
+++ frontend/src/ChatPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io("http://localhost:5000");
+const socket = io(import.meta.env.VITE_API_URL);
 
 const ChatPage = ({ room, myId }) => {
   const [message, setMessage] = useState("");
