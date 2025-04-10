be1dc4798e15cb903e2cc95e493bbb18d348ba12
yash gupta <yashgupta5046@gmail.com>
2025-04-10 23:09:06 +0530

    11th commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: true, // you can change this
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-icons/fa'],
-    },
+    host: true,
   },
 });
